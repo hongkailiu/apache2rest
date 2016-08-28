45e6adfdfcd92d43ee622f4158b8c433fd20aa7b
hongkailiu <hongkailiu@users.noreply.github.com>
2016-08-28 14:11:48 -0400

    add logstash conf file

--- test-springboot/src/main/java/tk/hongkailiu/test/entity/LogEntry.java
+++ test-springboot/src/main/java/tk/hongkailiu/test/entity/LogEntry.java
@@ -1,6 +1,7 @@
 package tk.hongkailiu.test.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.Date;
@@ -8,12 +9,19 @@ import java.util.Date;
 /**
  * Created by hongkailiu on 2016-08-25.
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data public class LogEntry {
     // 24/Jul/2016:18:55:45 -0400
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd/MMM/yyyy:HH:mm:ss Z", timezone="America/Montreal")
     private Date time;
-    private String level;
     private String agent;
     private String ip;
-    private String content;
+    private String request;
+    private String host;
+    private String ident;
+    private String auth;
+    private String verb;
+    private String httpVersion;
+    private String response;
+    private String bytes;
 }
